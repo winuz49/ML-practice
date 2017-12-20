a125b29bc81f7c046e30ca7b03328813fc1c43db
winuz49 <zjwinuj@163.com>
2017-12-20 20:49:07 +0800

    add train for speech recognition use keras

---
+++ speech_recognition/train.py
@@ -0,0 +1,19 @@
+import os
+import numpy as np
+from scipy.fftpack import fft
+from scipy.io import wavfile
+from scipy import signal
+from glob import glob
+import re
+import pandas as pd
+import gc
+from scipy.io import wavfile
+
+from keras import optimizers, losses, activations, models
+from keras.layers import Convolution2D, Dense, Input, Flatten, Dropout, MaxPooling2D, BatchNormalization
+from sklearn.model_selection import train_test_split
+import keras
+
+if __name__ == '__main__':
+    print(keras.__version__)
+    print('hello')
