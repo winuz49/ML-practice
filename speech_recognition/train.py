246173f76b695de6a70b722789ca8ec870facb21
winuz49 <zjwinuj@163.com>
2017-12-24 21:17:18 +0800

    modify rm

--- speech_recognition/train.py
+++ speech_recognition/train.py
@@ -14,6 +14,34 @@ from keras.layers import Convolution2D, Dense, Input, Flatten, Dropout, MaxPooli
 from sklearn.model_selection import train_test_split
 import keras
 
+L = 16000
+legal_labels = 'yes no up down left right on off stop go silence unknown'.split()
+
+train_data_path = os.path.join('./data', 'train', 'audio')
+test_data_path = os.path.join('./data', 'train', 'audio')
+
+def custom_fft(y, fs):
+    T = 1.0 / fs
+    N = y.shape[0]
+    yf = fft(y)
+    xf = np.linspace(0.0, 1.0/(2.0*T), N//2)
+    # FFT is simmetrical, so we take just the first half
+    # FFT is also complex, to we take just the real part (abs)
+    vals = 2.0/N * np.abs(yf[0:N//2])
+    return xf, vals
+
+def log_specgram(audio, sample_rate, window_size=20,
+                 step_size=10, eps=1e-10):
+    nperseg = int(round(window_size * sample_rate / 1e3))
+    noverlap = int(round(step_size * sample_rate / 1e3))
+    freqs, times, spec = signal.spectrogram(audio,
+                                    fs=sample_rate,
+                                    window='hann',
+                                    nperseg=nperseg,
+                                    noverlap=noverlap,
+                                    detrend=False)
+    return freqs, times, np.log(spec.T.astype(np.float32) + eps)
+
 if __name__ == '__main__':
-    print(keras.__version__)
-    print('hello')
+    print(train_data_path)
+    print(test_data_path)
