2d5ee681b5f94fced22b001dc91654adff447a65
winuz49 <zjwinuj@163.com>
2018-01-03 20:16:05 +0800

    add gradient descent

--- lhy_2017/hw1/GradientDescent.py
+++ lhy_2017/hw1/GradientDescent.py
@@ -1,12 +1,13 @@
 import numpy as np
 
-
+#TODO
 class GradientDescent(object):
-    def __init__(self):
-        self.__wt = None
-        self.__b = None
-        self.__x = None
-        self.__y = None
+    def __init__(self, wt, b, x, y, lr):
+        self.__wt = wt
+        self.__b = b
+        self.__x = x
+        self.__y = y
+        self.__lr = lr
 
     @property
     def wt(self):
@@ -16,12 +17,29 @@ class GradientDescent(object):
     def b(self):
         return self.__b
 
-    def train_by_pseudo_inverse(self, X, y, alpha=0, validate_data=None):
+    def train_by_pseudo_inverse(self, x, y, alpha=0, validate_data=None):
         return
 
-    def train(self,X,y,init_wt=np.array([]),init_b=0,rate=0.01,alpha=0,epoch=1000,batch=None,
+    def train(self,x,y,init_wt=np.array([]),init_b=0,rate=0.01,alpha=0,epoch=1000,batch=None,
                 validate_data=None):
         return
+
+    def step_gradient(self):
+        N = self.__x.shape()[0]
+        colums = self.__x.shape()[1]
+        w_gradient = np.zeros(colums)
+        b_gradient = 0
+
+        for i in range(0, N):
+            x = self.__x[i]
+            y = self.__y[i]
+            w_gradient += -2 * (y - np.dot(w_gradient, x)+b_gradient)*x
+            b_gradient += -2 * (y - np.dot(w_gradient, x)+b_gradient)
+
+        new_wt = self.__wt - self.__lr * w_gradient / N
+        new_b = self.__b - self.__lr * b_gradient / N
+
+        return [new_wt, new_b]
 
     def update(self, X, y, wt, b, rate, alpha):
         return
@@ -29,7 +47,7 @@ class GradientDescent(object):
     def predict(self, X):
         return
 
-    def err(self, X, y):
+    def compute_err(self, X, y):
         return
 
     def _check_data(self, X, y):
